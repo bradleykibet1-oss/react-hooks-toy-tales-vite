7d6fdd87dd0584044d6c9c3159c12878f4ef1a9b
Enock <enock.kibet@student.moringaschool.com>
2026-05-13 10:20:50 +0300

    Made the handle functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,44 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((toys) => setToys(toys));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(newToy) {
+    setToys((toys) => [...toys, newToy]);
+  }
+
+  function handleUpdateToy(updatedToy) {
+    setToys((toys) =>
+      toys.map((toy) => (toy.id === updatedToy.id ? updatedToy : toy))
+    );
+  }
+
+  function handleDeleteToy(id) {
+    setToys((toys) => toys.filter((toy) => toy.id !== id));
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer
+        toys={toys}
+        onUpdateToy={handleUpdateToy}
+        onDeleteToy={handleDeleteToy}
+      />
     </>
   );
 }
